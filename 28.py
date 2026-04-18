90bbf9e0437e7434f3f493d1749b9d8e9dfa5453
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-04-18 18:15:56 +0500

    add new algorithm files for input processing and calculations

---
+++ 28.py
@@ -0,0 +1,9 @@
+n = int(input())
+strings = []
+for i in range(n):
+    s = input()
+    strings.append(s)
+    if len(s) > 10:
+        print(s[0] + s[-1])
+    else:
+        print(s)
---
+++ 70.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+s = 0
+
+if a % 2 == 0: # 2 ga bo‘linishini tekshiradi
+    s += 1 # a ning 2 ga bo‘linishini tekshiradi va s ni 1 ga oshiradi
+
+if b % 2 == 0: # 2 ga bo‘linishini tekshiradi
+    s += 1 # b ning 2 ga bo‘linishini tekshiradi va s ni 1 ga oshiradi
+
+print(s)  
