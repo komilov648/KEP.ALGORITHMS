d090b7be818850741975472bd37e0fa9f6a2ea53
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-04-28 16:56:19 +0500

    add new implementation to count unique elements in a list

---
+++ 377.py
@@ -0,0 +1,10 @@
+# n = int(input())
+# a = list(map(int, input().split()))
+n = int(input())
+a = list(map(int, input().split()))
+counter = 0
+for element in a:
+    if a.count(element) == 2:
+            counter += 1
+
+print(int(counter / 2))
