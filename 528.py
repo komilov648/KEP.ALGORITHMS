4209adb0ae12ef13efc1b7156b3afd9eeede59c6
komilov Gulomjon <komilovgulomjon428@gmail.com>
2026-05-07 18:17:04 +0500

    add new algorithm files for mapping, filtering, and counting divisors

---
+++ 528.py
@@ -0,0 +1,2 @@
+def map_square(sequence):
+    return map(lambda n : n ** 2, sequence)
---
+++ 529.py
@@ -0,0 +1,7 @@
+def divisors_count(number):
+    count = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            count += 1
+
+    return count
